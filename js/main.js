c347663e08ad15b7b9ff8831ee61d0a0e578fc57
Tech-A08 <tech-a08@MacBook-Air-3.local>
2018-09-18 12:12:34 +0100

    functional reset of values once win query triggered

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const game = new Object();
   const cards = new Object();
-  let cardTotUser = Number([]);
-  let cardTotComp = Number([]);
+  game.cardTotUser = Number([]);
+  game.cardTotComp = Number([]);
+  game.playerScore = 0;
+  game.compScore = 0;
 
   cards.cardName = [['A',11],['2',2],['3',3],['4',4],['5',5],['6',6],['7',7],['8',8],
   ['9',9],['10',10],['J',10],['Q',10],['K',10]];
@@ -15,6 +17,9 @@ document.addEventListener('DOMContentLoaded', () => {
   game.dealButn = document.getElementsByClassName('Deal');
   //GATHERS THE DEAL BUTTON
   game.stayButn = document.getElementsByClassName('Stand');
+  //GATHERS THE RESTART BUTTON
+  game.resetButn = document.getElementsByClassName('gameRestart');
+
 
   // DEAL RANDOM CARDS FUNCTION
   game.DealUser = () =>
@@ -26,8 +31,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let card = Name[0] + Number(Val) + Suit;
     console.log(card);
 
-    cardTotUser += Val;
-    console.log(cardTotUser);
+    game.cardTotUser += Val;
+    console.log(game.cardTotUser);
 
   } ///END OF DEAL USER
 
@@ -40,29 +45,51 @@ document.addEventListener('DOMContentLoaded', () => {
     let card = Name[0] + Number(Val) + Suit;
     console.log(card);
 
-    cardTotComp += Val;
-    console.log(cardTotComp);
+    game.cardTotComp += Val;
+    console.log(game.cardTotComp);
 
     /////IF STATEMENT FOR GOING BUST
-    if (cardTotUser > 21 && cardTotComp > 21) {
+    if (game.cardTotUser > 21 && game.cardTotComp > 21) {
+      game.restart();
       alert('Both Players Bust, It\'s A Draw!');
-    }else if (cardTotUser > 21 && cardTotComp <=21) {
+    }else if (game.cardTotUser > 21 && game.cardTotComp <=21) {
+      game.compScore += 1;
+      game.restart();
       alert('Player Bust, Computer wins');
-    }else if (cardTotComp > 21 && cardTotUser <= 21) {
+    }else if (game.cardTotComp > 21 && game.cardTotUser <= 21) {
+      game.playerScore =+ 1;
+      game.restart();
       alert('Computer Bust, Player Wins');
-    }else if (cardTotComp === cardTotUser) {
-      alert('It\'s a Draw!');
     }
-
   } ///END OF DEALCOMP
+
+  //RESTART WITHOUT REMOVING SCORE FUNCTION
+  game.restart = () => {
+    game.cardTotUser = Number([]);
+    game.cardTotComp = Number([]);
+  }
+
+///RESTART GAME COMPLETELY FUNCTION
+  game.resetGame = () => {
+    game.cardTotUser = Number([]);
+    game.cardTotComp = Number([]);
+    game.playerScore = 0;
+    game.compScore = 0;
+  }
+
+  ///GAME RESET BUTTON INCLUDING SCORE RESET
+  game.resetButn[0].addEventListener('click', () => {
+    game.resetGame();
+  })
 
   //WHEN START BUTTON IS CLICKED, TWO RANDOM CARDS ARE PRESENTED
   game.startButn[0].addEventListener('click', () => {
     game.DealUser();
     game.DealUser();
-
+    console.log('Player first two cards^');
     game.DealComp();
     game.DealComp();
+    console.log('Computer first two cards^');
   });
 
   //WHEN DEAL BUTTON IS CLICKED, A RANDOM CARDS IS PRESENTED
@@ -74,22 +101,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
   ////STAY BUTTON FUNCTIONAL
   game.stayButn[0].addEventListener('click', () => {
-    if (cardTotUser === cardTotComp) {
-      alert('Both Players Bust, It\'s A Draw!')
-    }else if (cardTotUser > cardTotComp) {
-      alert('User wins.')
+    if (game.cardTotUser === 0 && game.cardTotComp === 0) {
+      game.restart();
+    }else if (game.cardTotUser === game.cardTotComp) {
+      game.restart();
+      alert('Both Players Bust, It\'s A Draw!');
+    }else if (game.cardTotUser > game.cardTotComp) {
+      game.playerScore += 1;
+      game.restart();
+      alert('User wins.');
     }else {
-      alert('Computer wins.')
+      game.compScore += 1;
+      game.restart();
+      alert('Computer wins.');
     }
+    ///SCORE TAG
+    console.log(`Player score is : ${game.playerScore}`);
+    console.log(`Computer score is : ${game.compScore}`);
   });
-
-
-
-
-
-
-
-
 
 
 }); //End of DOCUMENT Listener
